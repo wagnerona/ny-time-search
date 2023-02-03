1753eb0818b7f35a80e4667fd3173536e195bd9b
seacrest3 <o7716442@gmail.com>
2023-02-03 09:54:45 +0000

    established queryURL to NY times

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
+//NY Times Search
+
+let searchTerm = "";
+let numberRecords = "5";
+let startYear = "";
+let endYear = "";
 
 $('#run-search').on('click', function (event) {
   event.preventDefault();
 
-  let searchTerm = $('#search-term').val().trim()
-  let numberRecords = $('#article-count').val().trim()
-  let startYear = $('#start-year').val().trim()
-  let endYear = $('#end-year').val().trim()
+  searchTerm = $('#search-term').val().trim();
+  numberRecords = $('#article-count').val().trim();
+  startYear = $('#start-year').val().trim();
+  endYear = $('#end-year').val().trim();
 
   console.log(numberRecords);
-})
+
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=8uZlW4ti3KyZlYaruw98zWMq4mpcN0wG";
+
+  $.ajax({
+    url: queryURL,
+    method: "Get"
+  }).then(function (response) {
+    console.log(response);
+    
+    const results = response.response.docs;
+    console.log(results[0]);
+    for (var i = 0; i < results.length; i++) {
+      const articleDiv = $('<div>');
+      const pTag = $('<p>');
+      pTag.text('Article:' + JSON.stringify(results[i]));
+      articleDiv.append(pTag);
+      $('#search-form').append(articleDiv);
+      // console.log(results[i]);
+
+    }
+
+  });
+});
